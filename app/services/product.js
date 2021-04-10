de6cd2a3ebc9fbdcc6e4778082f2ea490645e769
tech-dev-cloud <amit.dcsa@gmail.com>
2021-04-10 19:18:40 +0530

    - bug fixes for video course

--- app/services/product.js
+++ app/services/product.js
@@ -4,12 +4,15 @@ const { VideoContent } = require("../models");
 let service = {
     getCourseContent: async (product) => {
         let selectedContentFields = ['_id', 'title', 'lectures'];
-        let lectureFields = ['isPreview', 'title', 'description', 'file_type', 'duration']
+        let lectureFields = ['isPreview', 'title', 'description', 'file_type', 'duration', 'url']
         let contents = await VideoContent.find({ product_id: product._id, status: true }).lean();
         product.contents = contents.map(content => {
             content = _.pick(content, selectedContentFields);
             content.lectures = content.lectures.map(lecture => {
                 lecture = _.pick(lecture, lectureFields);
+                if (!lecture.isPreview) {
+                    delete lecture.url;
+                }
                 return lecture;
             })
             content.lectureCounts = content.lectures.length;
