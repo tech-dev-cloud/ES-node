c05c0dfc2069bff5662fecf0d56ccb9a6ba2188b
tech-dev-cloud <amit.dcsa@gmail.com>
2021-07-11 00:43:10 +0530

    - add offer discount price in response

--- app/services/product.js
+++ app/services/product.js
@@ -250,6 +250,7 @@ let service = {
     applyEarlyBirdOffer(product){
         for(let index=0;index<product.early_birds_offer.length;index++){
             if(product.totalEnrolled<=product.early_birds_offer[index].enrolled_limit){
+                product.discountPrice=product.strikeprice-product.early_birds_offer[index].price;
                 product.price=product.early_birds_offer[index].price;
                 product['discountPercent'] = Math.ceil((product.strikeprice - product.price) * 100 / product.strikeprice);
                 break;
