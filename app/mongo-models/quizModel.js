de326bd0444fc6d8129de1bf2eacc821bf3575b3
tech-dev-cloud <amit.dcsa@gmail.com>
2021-07-21 20:39:30 +0530

    - Create CRUD for QUiz

--- app/mongo-models/quizModel.js
+++ app/mongo-models/quizModel.js
@@ -1,29 +1,22 @@
-const { PRODUCTS_TYPE, DIFFICULT_LEVEL } = require('../utils/constants');
+const { DIFFICULT_LEVEL } = require('../utils/constants');
 const MONGOOSE = require('mongoose');
 const Schema = MONGOOSE.Schema;
 
 let schema = new Schema({
   title: { type: String, required: true },
-  subjectId: { type: Schema.Types.ObjectId, required: true, ref: 'subjects' },
+  heading: { type: String },
+  subjectId: [{ type: Schema.Types.ObjectId, required: true, ref: 'subjects' }],
   status: { type: Boolean, default: true },
   examType: [{ type: Schema.Types.ObjectId, ref: 'examtype' }],
-
-  imageURL: { type: String },
-  isPaid: { type: Boolean, required: true },
-  amount: { type: Number },
-  instructor: { type: Schema.Types.ObjectId, ref: 'user' },
-  headline: { type: String },
+  createdBy: { type: Schema.Types.ObjectId, ref: 'user' },
   difficultLevel: { type: String, enum: Object.values(DIFFICULT_LEVEL) },
-  benefits: [{ type: String }],
-  // requirements: { type: String },
-  // description: { type: String },
-  productType: { type: String, enum: Object.values(PRODUCTS_TYPE), default: PRODUCTS_TYPE.QUIZ },
   questionList: [{ type: Schema.Types.ObjectId, ref: 'question' }],
   totalQuestions: { type: Number },
   attemptTime: { type: Number },
-  isDeleted: { type: Boolean, default: false }
+  isDeleted: { type: Boolean, default: false },
 });
 
 schema.set('timestamps', true);
+schema.index({ name: 'text', heading: 'text' });
 let QuizModel = MONGOOSE.model('quiz', schema);
 module.exports = { QuizModel };
--- app/routes/eduseeker/quizRoutes.js
+++ app/routes/eduseeker/quizRoutes.js
@@ -1,87 +1,80 @@
-
 const JOI = require('joi');
-const { USER_ROLE } = require('../../utils/constants');
+const { USER_ROLE, DIFFICULT_LEVEL } = require('../../utils/constants');
 const { quizController } = require('../../controllers');
 const routeUtils = require('../../utils/routeUtils');
 
 let MODULE = {
   name: 'quiz',
-  group: 'Quiz'
-}
+  group: 'Quiz',
+};
 const routes = [
-  // {
-  //   path: `/api/${MODULE.name}`,
-  //   method: 'POST',
-  //   joiSchemaForSwagger: {
-  //     headers: JOI.object({
-  //       'authorization': JOI.string().required()
-  //     }).unknown(),
-  //     body:{
-  //       title: JOI.string().required(),
-  //       subjectId: routeUtils.validation.mongooseId,
-  //       imageURL: JOI.string(),
-  //       isPaid:JOI.boolean().required(),
-  //       amount:JOI.number(),
-  //       headline:JOI.string(),
-  //       difficultLevel:JOI.string().valid(Object.values(CONSTANTS.DIFFICULT_LEVEL)),
-  //       questionList:JOI.array().items(routeUtils.validation.mongooseId),
-  //       attemptTime:JOI.number().required(),
-  //       // description:JOI.string().required(),
-  //       // requirements:JOI.string(),
-  //       benefits:JOI.array().items(JOI.string())
-  //     },
-  //     group: `${MODULE.group}`,
-  //     description: 'Api to create Quiz',
-  //     model: 'CreateQuiz'
-  //   },
-  //   auth:[USER_ROLE.TEACHER, USER_ROLE.ADMIN],
-  //   handler: quizController.createQuiz
-  // },
-  // {
-  //   path: `/api/${MODULE.name}/:quizId`,
-  //   method: 'PUT',
-  //   joiSchemaForSwagger: {
-  //     headers: JOI.object({
-  //       'authorization': JOI.string().required()
-  //     }).unknown(),
-  //     params:{
-  //       quizId: routeUtils.validation.mongooseId.required()
-  //     },
-  //     body:{
-  //       title: JOI.string(),
-  //       subjectId: routeUtils.validation.mongooseId,
-  //       imageURL: JOI.string(),
-  //       isPaid:JOI.boolean(),
-  //       amount:JOI.number(),
-  //       headline:JOI.string(),
-  //       difficultLevel:JOI.string().valid(Object.values(CONSTANTS.DIFFICULT_LEVEL)),
-  //       questionList:JOI.array().items(routeUtils.validation.mongooseId),
-  //       attemptTime:JOI.number(),
-  //       benefits:JOI.array().items(JOI.string())
-  //     },
-  //     group: `${MODULE.group}`,
-  //     description: 'Api to create Quiz',
-  //     model: 'CreateQuiz'
-  //   },
-  //   auth:[USER_ROLE.TEACHER, USER_ROLE.ADMIN],
-  //   handler: quizController.updateQuiz
-  // },
-  // {
-  //   path: `/api/${MODULE.name}`,
-  //   method: 'GET',
-  //   joiSchemaForSwagger: {
-      
-  //     query: {
-  //       index: JOI.number().default(DEFAULT.INDEX).min(DEFAULT.INDEX),
-  //       limit: JOI.number().min(DEFAULT.LIMIT).min(0)
-  //     },
-  //     group: `${MODULE.group}`,
-  //     description: 'Api to get Quiz List',
-  //     model: 'GetQuiz'
-  //   },
-  //   // auth: [USER_ROLE.TEACHER, USER_ROLE.ADMIN],
-  //   handler: quizController.findResource
-  // },
+  {
+    path: `/api/quiz`,
+    method: 'POST',
+    joiSchemaForSwagger: {
+      headers: JOI.object({
+        authorization: JOI.string().required(),
+      }).unknown(),
+      body: {
+        title: JOI.string().required(),
+        subjectId: routeUtils.validation.mongooseId,
+        headline: JOI.string(),
+        difficultLevel: JOI.string().valid(Object.values(DIFFICULT_LEVEL)),
+        questionList: JOI.array().items(routeUtils.validation.mongooseId),
+        attemptTime: JOI.number().required(),
+      },
+      group: `QUIZ`,
+      description: 'Api to create Quiz',
+      model: 'CreateQuiz',
+    },
+    auth: [USER_ROLE.TEACHER, USER_ROLE.ADMIN],
+    handler: quizController.createQuiz,
+  },
+  {
+    path: '/api/quiz/:quizId',
+    method: 'PUT',
+    joiSchemaForSwagger: {
+      headers: JOI.object({
+        authorization: JOI.string().required(),
+      }).unknown(),
+      params: {
+        quizId: routeUtils.validation.mongooseId.required(),
+      },
+      body: {
+        title: JOI.string(),
+        subjectId: routeUtils.validation.mongooseId,
+        headline: JOI.string(),
+        difficultLevel: JOI.string().valid(Object.values(DIFFICULT_LEVEL)),
+        questionList: JOI.array().items(routeUtils.validation.mongooseId),
+        attemptTime: JOI.number(),
+      },
+      group: `QUIZ`,
+      description: 'Api to Update Quiz',
+      model: 'UpdateQuiz',
+    },
+    auth: [USER_ROLE.TEACHER, USER_ROLE.ADMIN],
+    handler: quizController.updateQuiz,
+  },
+  {
+    path: `/api/quiz`,
+    method: 'GET',
+    joiSchemaForSwagger: {
+      headers: JOI.object({
+        authorization: JOI.string().required(),
+        admin: JOI.boolean().default(false),
+      }).unknown(),
+      query: {
+        skip: JOI.number(),
+        limit: JOI.number(),
+        searchString: JOI.string(),
+      },
+      group: `QUIZ`,
+      description: 'Api to get Quiz List',
+      model: 'GetQuiz',
+    },
+    auth: [USER_ROLE.TEACHER, USER_ROLE.ADMIN],
+    handler: quizController.getQuizList,
+  },
   // {
   //   path: `/api/${MODULE.name}/enrolled`,
   //   method: 'GET',
@@ -96,69 +89,69 @@ const routes = [
   //   auth: [USER_ROLE.STUDENT],
   //   handler: quizController.getEnrolledQuiz
   // },
-  // {
-  //   path: `/api/${MODULE.name}/:quizId`,
-  //   method: 'GET',
-  //   joiSchemaForSwagger: {
-  //     params: JOI.object({
-  //       quizId: routeUtils.validation.mongooseId
-  //     }),
-  //     group: `${MODULE.group}`,
-  //     description: 'Api to get Quiz List',
-  //     model: 'GetQuiz'
-  //   },
-  //   handler: quizController.findResourceById
-  // },
+  {
+    path: '/api/quiz/:quizId',
+    method: 'GET',
+    joiSchemaForSwagger: {
+      params: JOI.object({
+        quizId: routeUtils.validation.mongooseId,
+      }),
+      group: `QUIZ`,
+      description: 'Api to get Quiz by ID',
+      model: 'GetQuiz',
+    },
+    handler: quizController.getQuizByID,
+  },
   {
     path: `/api/${MODULE.name}/play/:product_id`,
     method: 'GET',
     joiSchemaForSwagger: {
       headers: JOI.object({
-        'authorization': JOI.string().required()
+        authorization: JOI.string().required(),
       }).unknown(),
       params: JOI.object({
-        product_id: routeUtils.validation.mongooseId
+        product_id: routeUtils.validation.mongooseId,
       }),
       group: `${MODULE.group}`,
       description: 'Api to get Quiz data to play',
-      model: 'GetQuizToPlay'
+      model: 'GetQuizToPlay',
     },
     auth: [USER_ROLE.STUDENT],
-    handler: quizController.getDataToPlay
+    handler: quizController.getDataToPlay,
   },
   {
-    path: `/api/${MODULE.name}/:quizId`,
+    path: `/api/quiz/:quizId`,
     method: 'DELETE',
     joiSchemaForSwagger: {
       headers: JOI.object({
-        'authorization': JOI.string().required()
+        authorization: JOI.string().required(),
       }).unknown(),
       params: JOI.object({
-        quizId: routeUtils.validation.mongooseId
+        quizId: routeUtils.validation.mongooseId,
       }),
-      group: `${MODULE.group}`,
+      group: 'QUIZ',
       description: 'Api to delete Quiz',
-      model: 'DeleteQuiz'
+      model: 'DeleteQuiz',
     },
     auth: [USER_ROLE.TEACHER, USER_ROLE.ADMIN],
-    handler: quizController.deleteQuiz
+    handler: quizController.deleteQuiz,
   },
   {
     path: `/api/flushCache/${MODULE.name}`,
     method: 'GET',
     joiSchemaForSwagger: {
       headers: JOI.object({
-        'authorization': JOI.string().required()
+        authorization: JOI.string().required(),
       }).unknown(),
-      query:JOI.object({
-        id:JOI.string()
+      query: JOI.object({
+        id: JOI.string(),
       }),
       group: `${MODULE.group}`,
       description: 'Api to flush all Quiz from Cache',
-      model: 'FlushQuizCache'
+      model: 'FlushQuizCache',
     },
     auth: [USER_ROLE.ADMIN, USER_ROLE.TEACHER],
-    handler: quizController.flushCache
-  }
-]
+    handler: quizController.flushCache,
+  },
+];
 module.exports = routes;
