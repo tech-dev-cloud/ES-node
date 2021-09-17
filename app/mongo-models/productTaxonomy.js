7dc84af2f0448a545aedfaaabc1ca0c46d35cde7
tech-dev-cloud <amit.dcsa@gmail.com>
2021-09-17 17:12:44 +0530

    - fixes

--- app/mongo-models/productTaxonomy.js
+++ app/mongo-models/productTaxonomy.js
@@ -3,8 +3,8 @@ const Schema = MONGOOSE.Schema;
 const schema = new Schema({
   productId: { type: Schema.Types.ObjectId, ref: 'products' },
   termId: { type: Schema.Types.ObjectId, ref: 'taxonomy' },
-  status: { type: Boolean, default:true },
+  status: { type: Boolean, default: true },
 });
 
-const ProductTaxonomy = model('product_taxonomy', schema);
+const ProductTaxonomy = MONGOOSE.model('product_taxonomy', schema);
 module.exports = { ProductTaxonomy };
